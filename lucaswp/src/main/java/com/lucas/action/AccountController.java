210f01a9e857b9ae4906904aee33dbb7ee3c0637
李江 <lijiang2@cufs.com.cn>
2019-01-16 10:36:47 +0800

    #2019-01-15 lucas 架构初始化

--- lucaswp/src/main/java/com/lucas/action/AccountController.java
+++ lucaswp/src/main/java/com/lucas/action/AccountController.java
@@ -4,8 +4,6 @@
  */
 package com.lucas.action;
 
-import com.lucas.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
 
-    @Autowired
-    private AccountService accountService;
-
     @RequestMapping("/account")
     public void findAccount(){
         System.out.println("controller been view");
-        accountService.initAccount();
     }
 
 }
