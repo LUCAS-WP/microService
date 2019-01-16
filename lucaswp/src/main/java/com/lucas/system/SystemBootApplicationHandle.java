95dfd6ec71f1df3148b606309dbfcd84cb75df0d
李江 <lijiang2@cufs.com.cn>
2019-01-16 10:34:51 +0800

    #2019-01-15 lucas 架构初始化

--- lucaswp/src/main/java/com/lucas/system/SystemBootApplicationHandle.java
+++ lucaswp/src/main/java/com/lucas/system/SystemBootApplicationHandle.java
@@ -7,7 +7,6 @@ package com.lucas.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by LUCAS.
