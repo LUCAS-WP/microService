d2d05d84896e768a6fa457492a56db9771a6eacf
李江 <lijiang2@cufs.com.cn>
2019-01-21 17:44:48 +0800

    #2019-01-15 lucas 架构初始化

--- micro-security/src/main/java/com/lucas/system/SystemBootApplicationHandle.java
+++ micro-security/src/main/java/com/lucas/system/SystemBootApplicationHandle.java
@@ -7,6 +7,8 @@ package com.lucas.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * Created by LUCAS.
@@ -22,6 +24,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //@EnableAutoConfiguration
 //@BootServiceRegister(basePackages = "com.lucas.*")
 @EnableDiscoveryClient
+//@EnableFeignClients(basePackages = {
+//        "com.micro.remote.client",
+//})
 public class SystemBootApplicationHandle {
 
     public static void main(String[] args) {
